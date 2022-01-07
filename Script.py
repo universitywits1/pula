78003f824b2507f5f4c2f225abf0b20a75f4dfd9
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-07 17:48:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -19,8 +19,11 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    
-
+SOURCE_TXT = """<b>NOTE:</b>
+- Eva Maria is a open source project. 
+- Source - https://github.com/EvamariaTG/EvaMaria  
+<b>DEVS:</b>
+- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""    
 <b>DEVS:</b>
 - <a href=https://t.me/MovieRosterGroup>Team Movie Roster</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
