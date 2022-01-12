83605822fbee9216a44e2d813982cad02a6404a6
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-01-12 10:09:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -73,10 +73,10 @@ This module only works for my admins
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+<b>★ 𝘛𝘰𝘵𝘢𝘭 𝘜𝘴𝘦𝘳𝘴 :</b> <code>{}</code>
+<b>★ 𝘛𝘰𝘵𝘢𝘭 𝘊𝘩𝘢𝘵𝘴 :</b> <code>{}</code>
+<b>★ 𝘜𝘴𝘦𝘥 𝘚𝘵𝘰𝘳𝘢𝘨𝘦 :</b> <code>{}</code> 𝙼𝚒𝙱
+<b>★ 𝘍𝘳𝘦𝘦 𝘚𝘵𝘰𝘳𝘢𝘨𝘦 :<b> <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
